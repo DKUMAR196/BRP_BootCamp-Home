fa76e05f5cbe0930c5e071e34c8018b33749d6e4
DKUMAR196 <84921216+DKUMAR196@users.noreply.github.com>
2021-10-12 09:20:11 +0530

    Initial commit: Common & weekly review programs are done in the Session

---
+++ common/Binary.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.program.common;
+
+import java.util.Scanner;
+
+public class Binary {
+	static void findBinary(int a, int n) {
+		int count = 0;
+		String x = "";
+		while(n > 0)
+		{			
+			a = n % 2;
+			if(a == 1)
+			{
+				count++;
+			}
+			x = a + "" + x;
+			n = n / 2;
+		}
+		System.out.println("Binary number: " +x);		
+	}
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter any decimal number:");
+		int n = sc.nextInt();
+		sc.close();
+		findBinary(n, n);
+	}
+}
+
+
